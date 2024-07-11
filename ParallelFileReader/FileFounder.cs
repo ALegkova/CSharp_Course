c20cdab8c3f642916856163b08e57b1f0bcfeef1
ALegkova <a_leg@mail.ru>
2024-07-11 13:57:18 +0300

    Домашнее задание по теме "Введение в параллелизм в .NET. Отличия процесса, потока, домена и таска"

---
+++ ParallelFileReader/FileFounder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFileReader
+{
+    public class FileFounder
+    {
+        private ILogger logger;
+
+        public FileFounder(ILogger logger)
+        {
+            this.logger = logger;
+        }    
+
+        /// <summary>
+        /// Получить список файлов в папке
+        /// </summary>
+        /// <param name="searchDirectory"></param>
+        /// <returns></returns>
+        public List<string> GetFiles(string searchDirectory)
+        {
+            if (!Directory.Exists(searchDirectory))
+                throw new DirectoryNotFoundException();
+            var files = Directory.GetFiles(searchDirectory, "*.*", SearchOption.AllDirectories);
+            List<string> fileList = files.ToList();
+            return fileList;
+        }
+
+        /// <summary>
+        /// Посчитать количество пробелов в файле
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public async Task<long> CalcSpacesInFileAsync(string filePath)
+        {
+            string fileText = await File.ReadAllTextAsync(filePath);
+            long spaceCount = fileText.LongCount(Char.IsWhiteSpace);
+            logger.Log($"Файл: {filePath}, количество пробелов: {spaceCount} \n");
+            return spaceCount;
+        }
+
+        /// <summary>
+        /// Посчитать количество пробелов в файле
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public long CalcSpacesInFile(string filePath)
+        {
+            string fileText = File.ReadAllText(filePath);
+            long spaceCount = fileText.LongCount(Char.IsWhiteSpace);
+            logger.Log($"Файл: {filePath}, количество пробелов: {spaceCount} \n");
+            return spaceCount;
+        }
+
+    }
+}
---
+++ ParallelFileReader/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using TaskFileReader;
+
+namespace Delegates
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string searchDirectory = AppDomain.CurrentDomain.BaseDirectory + "\\TestFiles";
+
+            ILogger logger = new ConsoleLogger();
+            FileFounder fileFounder = new FileFounder(logger);
+            TaskFileFounder taskFileFounder = new TaskFileFounder(fileFounder);
+            //Прочитать все файлы последовательно
+            taskFileFounder.FileFounderRun(searchDirectory);
+            //Прочитать три файла последовательно
+            //testFileFounder.FileFounderRun(searchDirectory,3);
+            //Прочитать все файлы параллельно
+            await taskFileFounder.FileFounderRunAsync(searchDirectory);
+            //Прочитать 3 файла параллельно
+            //await testFileFounder.FileFounderRunAsync(searchDirectory, 3);
+        }       
+    }
+}
+
+
